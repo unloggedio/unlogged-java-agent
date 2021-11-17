1b763fffba14876c7c39f0ff7800eb015c2df595
Parth Mudgal <artpar@gmial.com>
2021-11-17 12:35:26 +0530

    rsocket test works

---
+++ src/test/java/com/insidious/agent/rsocket/RsocketTest.java
@@ -0,0 +1,15 @@
+package com.insidious.agent.rsocket;
+
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import org.junit.Test;
+
+public class RsocketTest {
+
+
+    @Test
+    public void testRsocket() {
+
+
+    }
+}

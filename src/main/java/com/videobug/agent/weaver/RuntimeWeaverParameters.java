541534a28afc698b7380378be5b4fbc630a7a35c
Parth Mudgal <artpar@gmail.com>
2022-06-26 19:02:41 -0700

    use $HOME/.Videobug as default location for storing logs

--- src/main/java/com/videobug/agent/weaver/RuntimeWeaverParameters.java
+++ src/main/java/com/videobug/agent/weaver/RuntimeWeaverParameters.java
@@ -18,7 +18,7 @@ public class RuntimeWeaverParameters {
 
     private static final String[] SYSTEM_PACKAGES = {"sun/", "com/sun/", "java/", "javax/"};
     private static final String ARG_SEPARATOR = ",";
-    private static final String SELOGGER_DEFAULT_OUTPUT_DIR = "selogger-output-{time}";
+    private static final String SELOGGER_DEFAULT_OUTPUT_DIR = System.getProperty("user.home") + "/.VideoBug" + "/selogger-output-{time}";
 
     private static final Pattern timePattern = Pattern.compile(".*(\\{time:([^}]+)\\}).*");
     /**

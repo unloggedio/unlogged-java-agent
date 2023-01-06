a91e2790f51c40fa4a6281761d4c6c7866664311
Prakhar Porwal <13084389-prakhargl@users.noreply.gitlab.com>
2023-01-06 10:18:28 +0000

    handling case of Optional serialization with jackson

--- src/main/java/com/videobug/agent/logging/io/DetailedEventStreamAggregatedLogger.java
+++ src/main/java/com/videobug/agent/logging/io/DetailedEventStreamAggregatedLogger.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 //import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.google.gson.Gson;
 import com.insidious.common.weaver.ClassInfo;
 import com.insidious.common.weaver.DataInfo;
@@ -126,62 +127,14 @@ public class DetailedEventStreamAggregatedLogger implements IEventLogger {
             kryo = null;
             objectMapper = null;
             fstObjectMapper = null;
-//        } else if (SERIALIZATION_MODE == SerializationMode.JACKSON) {
-//            JsonMapper.Builder jacksonBuilder = JsonMapper.builder();
-//            jacksonBuilder.annotationIntrospector(new JacksonAnnotationIntrospector() {
-//                @Override
-//                public boolean hasIgnoreMarker(AnnotatedMember m) {
-////                    System.err.println("[" + m.getMember()
-////                            .getClass() + "]" + "Check hasIngore marker: " + m.getMember());
-////                    if (m.getMember() instanceof Method) {
-////                        return true;
-////                    }
-//                    return false;
-//                }
-//            });
-//            DateFormat df = new SimpleDateFormat("MMM d, yyyy HH:mm:ss aaa");
-//            jacksonBuilder.defaultDateFormat(df);
-//            jacksonBuilder.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-//            jacksonBuilder.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
-//            jacksonBuilder.configure(SerializationFeature.WRITE_SELF_REFERENCES_AS_NULL, true);
-//
-//            try {
-//                Class<?> hibernateModule = Class.forName("com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module");
-//                Hibernate5Module module = new Hibernate5Module();
-//                module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
-//                module.configure(Hibernate5Module.Feature.REPLACE_PERSISTENT_COLLECTIONS, true);
-//                jacksonBuilder.addModule(module);
-//            } catch (ClassNotFoundException e) {
-//                // hibernate module not found
-//                // add a warning in System.err here ?
-//            }
-//
-//
-//            try {
-//                Class<?> jodaModule = Class.forName("com.fasterxml.jackson.datatype.joda.JodaModule");
-//                jacksonBuilder.addModule((Module) jodaModule.getDeclaredConstructor()
-//                        .newInstance());
-////                System.err.println("Loaded JodaModule");
-//
-//            } catch (ClassNotFoundException e) {
-//                // joda not present
-////                e.printStackTrace();
-//            } catch (InvocationTargetException
-//                     | InstantiationException
-//                     | IllegalAccessException
-//                     | NoSuchMethodException e) {
-//                throw new RuntimeException(e);
-//            }
-//            objectMapper = jacksonBuilder.build();
-//            kryo = null;
-//            gson = null;
-//            fstObjectMapper = null;
         } else if (SERIALIZATION_MODE == SerializationMode.JACKSON) {
-            // 2.9.7
-            objectMapper = new ObjectMapper();
-            objectMapper.setAnnotationIntrospector(new JacksonAnnotationIntrospector() {
+            // For 2.13.1
+            JsonMapper.Builder jacksonBuilder = JsonMapper.builder();
+            jacksonBuilder.annotationIntrospector(new JacksonAnnotationIntrospector() {
                 @Override
                 public boolean hasIgnoreMarker(AnnotatedMember m) {
+//                    System.err.println("[" + m.getMember()
+//                            .getClass() + "]" + "Check hasIngore marker: " + m.getMember());
 //                    if (m.getMember() instanceof Method) {
 //                        return true;
 //                    }
@@ -189,17 +142,105 @@ public class DetailedEventStreamAggregatedLogger implements IEventLogger {
                 }
             });
             DateFormat df = new SimpleDateFormat("MMM d, yyyy HH:mm:ss aaa");
-            objectMapper.setDateFormat(df);
-            objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-            objectMapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
+            jacksonBuilder.defaultDateFormat(df);
+            jacksonBuilder.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            jacksonBuilder.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
+            jacksonBuilder.configure(SerializationFeature.WRITE_SELF_REFERENCES_AS_NULL, true);
 
-            Hibernate5Module module = new Hibernate5Module();
-            module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
-            module.configure(Hibernate5Module.Feature.REPLACE_PERSISTENT_COLLECTIONS, true);
-            objectMapper.registerModule(module);
+            try {
+                Class<?> hibernateModule = Class.forName("com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module");
+                Hibernate5Module module = new Hibernate5Module();
+                module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+                module.configure(Hibernate5Module.Feature.REPLACE_PERSISTENT_COLLECTIONS, true);
+                jacksonBuilder.addModule(module);
+            } catch (ClassNotFoundException e) {
+                // hibernate module not found
+                // add a warning in System.err here ?
+            }
+
+            try {
+                //checks for presence of this module class, if not present throws exception
+                Class<?> jdk8Module = Class.forName("com.fasterxml.jackson.datatype.jdk8.Jdk8Module");
+                jacksonBuilder.addModule(new Jdk8Module());
+            }catch (ClassNotFoundException e){
+                // jdk8 module not found
+            }
+
+
+            try {
+                Class<?> jodaModule = Class.forName("com.fasterxml.jackson.datatype.joda.JodaModule");
+                jacksonBuilder.addModule((Module) jodaModule.getDeclaredConstructor()
+                        .newInstance());
+//                System.err.println("Loaded JodaModule");
+
+            } catch (ClassNotFoundException e) {
+                // joda not present
+//                e.printStackTrace();
+            } catch (InvocationTargetException
+                     | InstantiationException
+                     | IllegalAccessException
+                     | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+            objectMapper = jacksonBuilder.build();
             kryo = null;
             gson = null;
             fstObjectMapper = null;
+//        } else if (SERIALIZATION_MODE == SerializationMode.JACKSON) {
+//            // 2.9.7
+//            objectMapper = new ObjectMapper();
+//            objectMapper.setAnnotationIntrospector(new JacksonAnnotationIntrospector() {
+//                @Override
+//                public boolean hasIgnoreMarker(AnnotatedMember m) {
+////                    if (m.getMember() instanceof Method) {
+////                        return true;
+////                    }
+//                    return false;
+//                }
+//            });
+//            DateFormat df = new SimpleDateFormat("MMM d, yyyy HH:mm:ss aaa");
+//            objectMapper.setDateFormat(df);
+//            objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+//            objectMapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
+//
+//
+//            try {
+//                Class<?> hibernateModule = Class.forName("com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module");
+//                Hibernate5Module module = new Hibernate5Module();
+//                module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+//                module.configure(Hibernate5Module.Feature.REPLACE_PERSISTENT_COLLECTIONS, true);
+//                objectMapper.registerModule(module);
+//            } catch (ClassNotFoundException e) {
+//                // hibernate module not found
+//                // add a warning in System.err here ?
+//            }
+//
+//            try {
+//                //checks for presence of this module class, if not present throws exception
+//                Class<?> jdk8Module = Class.forName("com.fasterxml.jackson.datatype.jdk8.Jdk8Module");
+//                objectMapper.registerModule(new Jdk8Module());
+//             } catch (ClassNotFoundException e){
+//                // jdk8 module not found
+//             }
+//
+//            try {
+//                Class<?> jodaModule = Class.forName("com.fasterxml.jackson.datatype.joda.JodaModule");
+//                objectMapper.registerModule((Module) jodaModule.getDeclaredConstructor()
+//                        .newInstance());
+////                System.err.println("Loaded JodaModule");
+//             } catch (ClassNotFoundException e) {
+//                // joda not present
+////                e.printStackTrace();
+//              } catch (InvocationTargetException
+//                     | InstantiationException
+//                     | IllegalAccessException
+//                     | NoSuchMethodException e) {
+//                  throw new RuntimeException(e);
+//              }
+//
+//            kryo = null;
+//            gson = null;
+//            fstObjectMapper = null;
         } else if (SERIALIZATION_MODE == SerializationMode.FST) {
 
             FSTConfiguration defaultConfigMapper = FSTConfiguration.createDefaultConfiguration();
---
+++ src/test/java/com/videobug/agent/logging/io/TestDetailedEventStreamAggregatedLogger.java
@@ -0,0 +1,73 @@
+package com.videobug.agent.logging.io;
+
+import com.sun.javafx.logging.JFRInputEvent;
+import com.videobug.agent.logging.Logging;
+import com.videobug.agent.logging.perthread.PerThreadBinaryFileAggregatedLogger;
+import com.videobug.agent.logging.perthread.RawFileCollector;
+import com.videobug.agent.logging.util.FileNameGenerator;
+import com.videobug.agent.weaver.WeaveClassLoader;
+import com.videobug.agent.weaver.WeaveConfig;
+import com.videobug.agent.weaver.Weaver;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+// todo : complete this test
+public class TestDetailedEventStreamAggregatedLogger {
+    private Optional<?> createAOptionalValue() {
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(3);
+        list.add(1);
+        list.add(5);
+        return Optional.of(list);
+    }
+
+    private DetailedEventStreamAggregatedLogger createDetailedLogger() throws IOException {
+        WeaveConfig config = new WeaveConfig(WeaveConfig.KEY_RECORD_EXEC,
+                "localhost:9921", "username", "password");
+        WeaveClassLoader loader = new WeaveClassLoader(config);
+
+        File outputDir = new File("./test/");
+        outputDir.mkdirs();
+        Weaver weaver = new Weaver(outputDir, config);
+
+        FileNameGenerator fileNameGenerator = new FileNameGenerator(outputDir, "log-", ".selog");
+        RawFileCollector fileCollector = new RawFileCollector(64,
+                new FileNameGenerator(outputDir, "index-", ".zip"), null, weaver);
+        PerThreadBinaryFileAggregatedLogger perThreadBinaryFileAggregatedLogger
+                = new PerThreadBinaryFileAggregatedLogger(fileNameGenerator, weaver, fileCollector);
+
+        DetailedEventStreamAggregatedLogger detailedLogger = Logging.initialiseDetailedAggregatedLogger(
+                "com.some.package", perThreadBinaryFileAggregatedLogger, outputDir);
+        return detailedLogger;
+    }
+
+    @Test
+    @Ignore
+    public void recordOptionalObject_serializationShouldContainActualValue() {
+//        Object value = createAOptionalValue();
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(3);
+        list.add(1);
+        list.add(5);
+
+        DetailedEventStreamAggregatedLogger detailedLogger;
+        try {
+            detailedLogger = createDetailedLogger();
+            int dataId = 12;
+            detailedLogger.recordEvent(dataId, list);
+
+        } catch (Exception e) {
+            System.out.println("failed to create detailed logger!");
+        }
+
+
+    }
+}
